e6c93ef63378e1ef9b4dfb73f9938a3794930d81
Joel Shepherd <joel@swivid.ink>
2017-08-21 15:43:46 +1000

    Adding directory filter

--- src/app/manager/details.component.ts
+++ src/app/manager/details.component.ts
@@ -1,18 +1,33 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/combineLatest';
+import 'rxjs/add/operator/map';
 
-import { ManagerService } from "./manager.service";
-import { Directory } from "../filesystem/directory";
-import { File } from "../filesystem/file";
+import { Directory } from '../filesystem/directory';
+import { File } from '../filesystem/file';
+import { ManagerService } from './manager.service';
 
 @Component({
   selector: 'details-view',
   templateUrl: 'details.component.html'
 })
 export class DetailsComponent {
+  files: File[];
+  query: Subject<string> = new Subject();
 
-  constructor(
-    protected manager: ManagerService
-  ) { }
+  constructor(protected manager: ManagerService) {}
+
+  ngOnInit() {
+    this.query
+      .map(query => query.toLowerCase())
+      .combineLatest(this.manager.directory$)
+      .subscribe(([query, directory]) => {
+        this.files = directory.files
+          .filter((file) => !query || file.name.toLowerCase().includes(query));
+      });
+
+    this.query.next('');
+  }
 
   get directory(): Directory {
     return this.manager.directory;
